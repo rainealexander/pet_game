a8911266a41984fc5933078bc3dc2eabae792092
taadams1 <rainedoescode@gmail.com>
2022-04-28 23:08:06 -0500

    Progress on behaviors and state

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,17 @@ export const TICK_RATE = 3000;
 export const ICONS = ['fish', 'poop', 'weather'];
 export const RAIN_CHANCE = 0.25;
 export const SCENES = ['day', 'rain'];
-export const DAY_LENGTH = 3;
-export const NIGHT_LENGTH = 2;
+export const DAY_LENGTH = 60;
+export const NIGHT_LENGTH = 5;
+
+export const getNextHungerTime = (clock) => {
+  return Math.floor(Math.random() * 3) + 5 + clock;
+};
+
+export const getNextDeathTime = (clock) => {
+  return Math.floor(Math.random() * 4) + 3 + clock;
+};
+
+export const getNextPoopTime = (clock) => {
+  return Math.floor(Math.random() * 3) + 4 + clock;
+};
--- src/gameState.js
+++ src/gameState.js
@@ -1,11 +1,15 @@
 import { modFox, modScene } from "./ui";
-import { RAIN_CHANCE, SCENES, DAY_LENGTH, NIGHT_LENGTH } from "./constants";
+import { RAIN_CHANCE, SCENES, DAY_LENGTH, NIGHT_LENGTH, getNextHungerTime, getNextDeathTime, getNextPoopTime } from "./constants";
 
 const gameState = {
   current: 'INIT',
   clock: 1,
   wakeTime: -1,
   sleepTime: -1,
+  hungryTime: -1,
+  deathTime: -1,
+  poopTime: -1,
+  timeTillCelebrate: -1,
   tick() {
     this.clock++;
     console.log('clock: ', this.clock);
@@ -14,7 +18,12 @@ const gameState = {
       this.wake();
     } else if (this.clock === this.sleepTime) {
       this.sleep();
+    } else if (this.clock === this.hungryTime) {
+      this.getHungry();
+    } else if (this.clock === this.deathTime) {
+      this.die()
     }
+
     return this.clock;
   },
   startGame() {
@@ -30,12 +39,22 @@ const gameState = {
     this.scene = Math.random() > RAIN_CHANCE ? 0 : 1;
     modScene(SCENES[this.scene]);
     this.sleepTime = this.clock + DAY_LENGTH;
+    this.hungryTime = getNextHungerTime(this.clock);
   },
   sleep() {
     this.state = 'SLEEP';
     modFox('sleep');
     modScene('night');
     this.wakeTime = this.clock + NIGHT_LENGTH;
+  },
+  getHungry() {
+    this.current = 'HUNGRY';
+    this.deathTime = getNextDeathTime(this.clock);
+    this.hungryTime = -1;
+    modFox('hungry');
+  },
+  die() {
+    console.log('the reaper\'s embrace');
   },
   handleUserAction(icon) {
     // pet can't act in these states
@@ -68,7 +87,14 @@ const gameState = {
     console.log('cleanUpPoop')
   },
   feed() {
-    console.log('feed')
+    if (this.current !== 'HUNGRY') {
+      return;
+    }
+    this.current = 'FEEDING';
+    this.deathTime = -1;
+    this.poopTime = getNextPoopTime(this.clock);
+    modFox('eating');
+    this.timeTillCelebrate = this.clock + 2;
   },
 };
 
