30806b20004590af4c7f2fb82685d8d62cc43891
taadams1 <rainedoescode@gmail.com>
2022-04-28 21:27:27 -0500

    Interactions with UI

---
+++ src/init.js
@@ -0,0 +1,26 @@
+import game from "./gameState";
+import { TICK_RATE } from "./constants";
+import initButtons from "./buttons";
+
+async function init () {
+  console.log('Starting Game');
+  
+  initButtons(game.handleUserAction);
+
+  let nextTimeToTick = Date.now();
+  
+  function nextAnimationFrame () {
+    const now = Date.now();
+
+    if (nextTimeToTick <= now) {
+      game.tick();
+      nextTimeToTick = now + TICK_RATE;
+    }
+
+    requestAnimationFrame(nextAnimationFrame);
+  }
+
+  requestAnimationFrame(nextAnimationFrame);
+}
+
+init();
